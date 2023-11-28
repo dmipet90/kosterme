499771638b63a5c34e1cdb64060dd7d5839d8cf3
HGRUPP\dmitri.petrussevits <dmitri.petrussevits@helmes.com>
2023-11-28 10:08:39 +0200

    push services

---
+++ account-management-service/src/test/java/com/digitalcomanetwork/kosterme/accountmanagementservice/AccountManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalcomanetwork.kosterme.accountmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-service/src/main/java/com/digitalcomanetwork/kosterme/userservice/ui/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.digitalcomanetwork.kosterme.userservice.ui.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/status/check")
+    public String getStatus() {
+        return "Working!";
+    }
+}
